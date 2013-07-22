5710b107e48164c84c532e1434782bf7c778ee0c
XDA Garwynn <xdagarwynn@gmail.com>
2013-07-22 09:38:59 -0500

    Fix - working version

--- full_jfltecsp.mk
+++ full_jfltecsp.mk
@@ -24,7 +24,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-# Inherit from jflteusc device
+# Inherit from jfltecsp device
 $(call inherit-product, device/samsung/jfltecsp/device.mk)
 
 # Set those variables here to overwrite the inherited values.
